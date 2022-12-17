449dc1c4048a542d659619aa1a78ecb876168d2f
Nimeshmodi <modinimesh81@gmail.com>
2022-12-17 17:08:59 +0000

    version1

---
+++ ArithmeticOperators.java
@@ -0,0 +1,13 @@
+package NimeshHomework;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a=30;
+        int b=50;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        
+    }
+}
