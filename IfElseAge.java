12dee7087ee13e680a80f5bb98514b8f507e5fad
Nimeshmodi <modinimesh81@gmail.com>
2022-12-17 16:55:47 +0000

    first commit

---
+++ IfElseAge.java
@@ -0,0 +1,12 @@
+package NimeshHomework;
+
+public class IfElseAge {
+    public static void main(String[] args) {
+        int a=18;
+        if(a>18) {
+            System.out.println("a is bigger value");
+        }else{
+            System.out.println("18 is smaller value");
+        }
+    }
+}
